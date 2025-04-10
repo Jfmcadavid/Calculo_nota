1213bf512665ffacc5220efd7e7659a386bdf8c7
Jfmcadavid <johncadavid29@gmail.com>
2025-04-09 20:21:28 -0500

    calculo de nota

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /*desarollar un sistema de notas para un estudiante que desea saber su nota final de cada materia*/
+        /*El sistema debe pedir: nombre del estudiante, documento, programa en el cual esta matriculado (menu de opciones: desarrollo de software, diseño grafico, gastronomia)
+        el calculo de notas es de lam siguiente manera:
+        debe permitir seleccionar la materia(menu de opciones):
+        Desarrollo: Lógica, Metodología, Introducción
+        Gastronomia: Costos, panadería dulce, cocina internacional
+        Diseño: Tipografía, Iconografía, Semantica
+        El calculo de las materias se es de la siguiente manera:
+        * Para cada matería se evalua:
+            -examen parcial que equivale al 20% de la nota.
+            -Taller que equivale al 30% de la nota
+            -Examen Final equivalente al 50% de la nota
+
+        * De acuerdo al promedio determinar
+            - Si la nota esta entre 0 y 2.0 el estudiante pierde sin opcion a plan de mejoramiento
+            - Si la nota esta entre 2.1 y 2.9 el estudiante tendra la opcion de plan de mejoramiento calificando con maxima calificacion 3.0
+            - Si la nota es igual o superior a 3.0 esta sera la nota definitiva de la materia
+         */
+    }
+}
